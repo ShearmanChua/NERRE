012529d4cdf67cb3e2484f54260abb6066359c0c
test-dan-run <daniel.leongsj@gmail.com>
2022-06-03 15:33:16 +0800

    modularize code

--- inference.py
+++ inference.py
@@ -1,6 +1,6 @@
 import time
 import requests
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 import torch
 import flair
@@ -51,6 +51,7 @@ def generate_named_entities(tagger: SequenceTagger, text: str) -> List[Dict[str,
         ent['start_pos'] = start_pos
         ent['end_pos'] = end_pos
         ent['text'] = text[start_pos:end_pos]
+        ent['type'] = entity.tag
 
         entities.append(ent)
         el_temps.append({
@@ -61,46 +62,66 @@ def generate_named_entities(tagger: SequenceTagger, text: str) -> List[Dict[str,
 
     return entities, el_temps
 
-# load tagger
-tagger = load_tagger("flair/ner-english-large")
-model = load_re_model('wiki80_bert_softmax', device='cpu')
+def extract_linked_entities(
+    el_candidates: List[str], 
+    entities: List[Dict[str, str]]) -> List[Dict[str, str]]:
+    return [ent for cand, ent in zip(el_candidates, entities) if cand is not None]
 
-start1 = time.time()
+def generate_head_tail_pairs(entities: List[Dict[str, str]]) -> List[Dict[str, str]]:
 
-entities, el_templates = generate_named_entities(tagger, text)
+    head_tail_pairs = []
 
-end1 = time.time()
-print('Time taken for NER:', end1-start1)
+    for head_idx in range(0,len(entities)):
+        head_span = (entities[head_idx]['start_pos'],entities[head_idx]['end_pos'])
+        head_text = entities[head_idx]['text']
+        for tail_idx in range(head_idx+1,len(entities)):
+            tail_span = (entities[tail_idx]['start_pos'],entities[tail_idx]['end_pos'])
+            tail_text = entities[tail_idx]['text']
+            if head_text != tail_text:
+                head_tail_pairs.append({'head_span':head_span,'tail_span':tail_span,'head_text':head_text,'tail_text':tail_text})
 
-top_candidate_list, resolved_entities = predict_templates(el_templates)
+    return head_tail_pairs
 
-end2 = time.time()
-print('Time taken for EL:', end2-end1)
-print(resolved_entities)
+def extract_relations(re_model: opennre.model.SoftmaxNN, head_tail_pairs: List[Dict[str, str]], confidence_threshold: float = 0.7) -> List[Tuple[str]]:
 
-filtered_entities = [ent for cand, ent in zip(top_candidate_list, entities) if cand is not None]
-print(filtered_entities)
+    relations = []
 
-head_tail_pairs = []
+    for pair in head_tail_pairs:
+        relation, confidence = re_model.infer({'text': text, 'h': {'pos': pair['head_span']}, 't': {'pos': pair['tail_span']}})
+        if confidence > confidence_threshold:
+            relations.append((pair['head_text'], relation, pair['tail_text']))
+    
+    return relations
 
-for head_idx in range(0,len(filtered_entities)):
-    head_span = (filtered_entities[head_idx]['start_pos'],filtered_entities[head_idx]['end_pos'])
-    head_text = filtered_entities[head_idx]['text']
-    for tail_idx in range(head_idx+1,len(filtered_entities)):
-        tail_span = (filtered_entities[tail_idx]['start_pos'],filtered_entities[tail_idx]['end_pos'])
-        tail_text = filtered_entities[tail_idx]['text']
-        if head_text != tail_text:
-            head_tail_pairs.append({'head_span':head_span,'tail_span':tail_span,'head_text':head_text,'tail_text':tail_text})
+def do_everything(text: str):
+    # load tagger
+    tagger = load_tagger("flair/ner-english-large")
+    re_model = load_re_model('wiki80_bert_softmax', device='cpu')
 
-print(head_tail_pairs)
-end1 = time.time()
-print('time taken for pair population:', end1-start)
+    start1 = time.time()
 
-for pair in head_tail_pairs:
-    results = model.infer({'text': text, 'h': {'pos': pair['head_span']}, 't': {'pos': pair['tail_span']}})
-    if results[1] > 0.7:
-        print(results)
-        print(pair['head_text'],results[0],pair['tail_text'])
+    entities, el_templates = generate_named_entities(tagger, text)
 
-end = time.time()
-print('time taken for RE', end-start)
+    end1 = time.time()
+    print('Time taken for NER:', end1-start1)
+
+    top_candidate_list, resolved_entities = predict_templates(el_templates)
+
+    end2 = time.time()
+    print('Time taken for EL:', end2-end1)
+    print(resolved_entities)
+
+    linked_entities = extract_linked_entities(top_candidate_list, entities)
+    head_tail_pairs = generate_head_tail_pairs(linked_entities)
+
+    end3 = time.time()
+    print('time taken for generating head-tail pairs:', end3-end2)
+
+    relations = extract_relations(re_model, head_tail_pairs)
+
+    end4 = time.time()
+    print('time taken for RE', end4-end3)
+
+
+if __name__ == '__main__':
+
